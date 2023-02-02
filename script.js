fa993c875c7dd10224c026d76a977c133d80248c
George <georgeoustapas@gmail.com>
2023-02-02 19:38:27 +0000

    added event listener from the search button

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 let APIKey = "f44cfcc45327fddc443fa6767211b14b";
-let city;
+let searchButton = document.querySelector("#search-button");
+let searchCity = document.querySelector("#search-input");
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey)
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey);
 
+searchButton.addEventListener("click",function(event){
+    event.preventDefault();
+    let city = searchCity.value.trim();
+    
+    fetchCity(city);
+})
+
+// event listener for the fetch response 
+
+
+function fetchCity(city) {
+    if (city =! ""){
+
+        fetch("http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=" + APIKey)
+        .then(response => response.json())
+        .then(dataOfCity => {
+            let userInput = dataOfCity[0]; 
+            let cityLat = userInput.lat;
+            let cityLon = userInput.lon;
+            console.log(userInput.lat);
+            console.log(userInput.lon);
+            
+            return fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon="+cityLon +"&appid=" + APIKey)
+            
+        })
+        
+        .then(response => response.json())
+        .then(weatherOfCity => {
+            // let userInput = weatherOfCity[0]; 
+            console.log(weatherOfCity);
+            
+        })
+    }
+    
+}
