91a0c8e83043511d0208687337c9ffbccafec947
George <georgeoustapas@gmail.com>
2023-02-02 20:55:05 +0000

    corrected if statemnet inside fetch

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 let APIKey = "f44cfcc45327fddc443fa6767211b14b";
-let searchButton = document.querySelector("#search-button");
-let searchCity = document.querySelector("#search-input");
+let searchButtonEl = document.querySelector("#search-button");
+let searchCityEl = document.querySelector("#search-input");
 
 
+let city = "";
 
-searchButton.addEventListener("click",function(event){
+searchButtonEl.addEventListener("click",function(event){
     event.preventDefault();
-    let city = searchCity.value.trim();
+    city = searchCityEl.value.trim();
     
     fetchCity(city);
+    console.log(`city `, city );
 })
 
 // event listener for the fetch response 
 
 
 function fetchCity(city) {
-    if (city =! ""){
+    if (city != ""){
 
         fetch("http://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=" + APIKey)
         .then(response => response.json())
         .then(dataOfCity => {
+            console.log(dataOfCity);
+            console.log(`fetch city`,city);
             let userInput = dataOfCity[0]; 
             let cityLat = userInput.lat;
             let cityLon = userInput.lon;
